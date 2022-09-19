511672d86620aae690319511c12b0a4d399f29b0
Henry Chu <henrytech@outlook.com>
2022-09-19 15:31:03 +0800

    Added MethodNode.isJUnitTestCase

--- src/jvmMain/kotlin/net/henryhc/redstone/asm/Asm.kt
+++ src/jvmMain/kotlin/net/henryhc/redstone/asm/Asm.kt
@@ -3,6 +3,7 @@ package net.henryhc.redstone.asm
 import okio.BufferedSource
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
 
 /**
  * Reads the stream and parses its content as a [ClassNode].
@@ -11,3 +12,11 @@ import org.objectweb.asm.tree.ClassNode
  */
 fun BufferedSource.readAsClassCode(parsingOptions: Int = 0) =
     ClassNode().apply { ClassReader(inputStream()).accept(this, parsingOptions) }
+
+
+private val jUnitTestAnnotationDescriptors = setOf("Lorg/junit/Test;", "Lorg/junit/jupiter/api/Test;")
+
+/**
+ * Tells whether a [MethodNode] is a JUnit test case.
+ */
+val MethodNode.isJUnitTestCase: Boolean get() = this.visibleAnnotations?.any { it.desc in jUnitTestAnnotationDescriptors } == true
